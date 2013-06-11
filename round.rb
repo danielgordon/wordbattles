f107048b750f71c5786269402e78a485b01d3782
Daniel Gordon <danielgordon86@gmail.com>
2013-06-10 23:54:35 -0400

    Words now go into buckets for length 4, 5...13+.

--- round.rb
+++ round.rb
@@ -5,15 +5,32 @@ require './dictionary.rb'
 class Round
   attr_accessor :score
 
-#TODO: WB uses buckets of 4 to 13+.  This doesn't account for that. 
+  module WordResult
+    WORD_OK = 0
+    WORD_TOO_SHORT = 1
+    WORD_TOO_MANY_OF_LENGTH = 2
+    WORD_ALREADY_PLAYED = 3
+    WORD_INVALID = 4
+  end
+
   def initialize(file)
     @score = 0
-    @words_played = Array.new()
+    @words_played = Array.new(10) { Array.new() }
     @word_dictionary = Dictionary.new(file)
   end
 
+  def word_bucket(word)
+    bucket = -1 
+    if (word.length > 13)
+      bucket = 9
+    elsif (word.length > 3)
+      bucket = word.length - 4
+    end
+    return bucket
+  end
+
   def word_played?(word)
-    @words_played.each do |played_word|
+    @words_played[word_bucket(word)].each do |played_word|
       if played_word == word
         return true
       end
@@ -21,14 +38,8 @@ class Round
     return false
   end
   
-  def words_of_length(length)
-    count = 0
-    @words_played.each do |played_word|
-      if played_word.length == length
-        count += 1
-      end
-    end
-    return count
+  def words_of_length(word)
+    return @words_played[word_bucket(word)].length
   end
 
   def word_valid?(word)
@@ -37,21 +48,20 @@ class Round
 
   def add_word(word)
     add_points(word)
-    @words_played << word
+    @words_played[word_bucket(word)] << word
   end
 
   def add_points(word)
-    @score += 30 + (words_of_length(word.length)) * 30
+    @score += 30 + (words_of_length(word)) * 30
   end
 
   def check_word(word)
-    len = word.length
 #TODO: doesn't tell you what's wrong with the word, fix
-    if ((len > 3) and (words_of_length(len) < 3) and (not word_played?(word)) \
+    len = word.length
+    if ((len > 3) and (words_of_length(word) < 3) and (not word_played?(word)) \
       and (word_valid?(word)))
       
       add_word(word)
-      return true
     else
       return false 
     end 
